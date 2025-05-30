7b19bdb24d61df6b4450214acc92c4f994de919c
Shohelrana63 <shohelrana.beg@gmail.com>
2025-05-30 20:33:50 +0600

    done

---
+++ src/Components/CommonComponent/ImageLayout.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+
+const ImageLayout = ({ imageSrc }) => {
+    return (
+        <div>
+            <div class="flex items-start gap-2.5">
+                <div class="flex flex-col gap-1">
+                    <div class="flex flex-col w-full max-w-[326px] leading-1.5 p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-700">
+                        <div class="grid gap-4 grid-cols-2 my-2.5">
+                            {imageSrc?.map((img) => {
+                                return (<div class="group relative">
+                                    <div class="absolute w-full h-full bg-gray-900/50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-lg flex items-center justify-center">
+                                        <button data-tooltip-target="download-image-1" class="inline-flex items-center justify-center rounded-full h-8 w-8 bg-white/30 hover:bg-white/50 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50">
+                                            <svg class="w-4 h-4 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 18">
+                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 1v11m0 0 4-4m-4 4L4 8m11 4v3a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-3" />
+                                            </svg>
+                                        </button>
+                                        <div id="download-image-1" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-xs opacity-0 tooltip dark:bg-gray-700">
+                                            Download image
+                                            <div class="tooltip-arrow" data-popper-arrow></div>
+                                        </div>
+                                    </div>
+                                    <img src={img} class="rounded-lg" />
+                                </div>)
+                            })}
+
+
+                        </div>
+
+                    </div>
+                </div>
+                <button id="dropdownMenuIconButton" data-dropdown-toggle="dropdownDots" data-dropdown-placement="bottom-start" class="inline-flex self-center items-center p-2 text-sm font-medium text-center text-gray-900 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-800 dark:focus:ring-gray-600" type="button">
+                    <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 4 15">
+                        <path d="M3.5 1.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 6.041a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 5.959a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z" />
+                    </svg>
+                </button>
+                <div id="dropdownDots" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-40 dark:bg-gray-700 dark:divide-gray-600">
+                    <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownMenuIconButton">
+                        <li>
+                            <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Reply</a>
+                        </li>
+                        <li>
+                            <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Forward</a>
+                        </li>
+                        <li>
+                            <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Copy</a>
+                        </li>
+                        <li>
+                            <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Report</a>
+                        </li>
+                        <li>
+                            <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Delete</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default ImageLayout
--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Group from "../../Components/HomeComponents/Group";
 import Friends from "../../Components/HomeComponents/Friends";
 import { HiDotsVertical } from "react-icons/hi";
@@ -9,18 +9,42 @@ import EmojiPicker from "emoji-picker-react";
 import { useSelector } from "react-redux";
 import { getDatabase, onValue, push, ref } from "firebase/database";
 import { getAuth } from "firebase/auth";
-
+import Modal from 'react-modal';
 import lib from "../../lib/lib";
-
+import { uploadCloudinaryFile } from "../../utils/cloudinary.utils";
+import ImageLayout from "../../Components/CommonComponent/ImageLayout";
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    width: '40%'
+  },
+};
 const Chat = () => {
   const [msg, setmsg] = useState("");
   const db = getDatabase();
   const auth = getAuth();
   const [emojiOpen, setemojiOpen] = useState(false);
-  const [allMsg, setAllmsg] = useState([]);
+  const [allMsg, setAllmsg] = useState(JSON.parse(localStorage.getItem('nesmsg')));
   const [loading, setloading] = useState(false);
   const { value: user } = useSelector((store) => store.friend);
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+  const [fieldImage, setFieldImage] = useState([])
+  const inputRef = useRef();
+  const [uploadImageLoading, setuploadImageLoading] = useState(false)
+  const [remove, setremove] = useState(false);
+  const [selectedMsg, setselectedMsg] = useState(null)
+  function openModal() {
+    setIsOpen(true);
+  }
 
+  function closeModal() {
+    setIsOpen(false);
+  }
   useEffect(() => {
     const fetchSingleMsg = async () => {
       const dbref = ref(db, "singlemsg");
@@ -65,10 +89,65 @@ const Chat = () => {
       setmsg("");
       setemojiOpen(false);
       setloading(false);
+
     }
   };
 
-  console.log(allMsg);
+
+  // handleUploadImage
+  const handleUploadImage = async () => {
+    setuploadImageLoading(true)
+    try {
+      // try to upload
+      const allImage = Array.from(fieldImage[0])
+      const allSecured_url = allImage.map(async (singleImage) => {
+        const formData = new FormData();
+        formData.append("file", singleImage);
+        formData.append("upload_preset", import.meta.env.VITE_UPLOAD_PRESET);
+        const url = await uploadCloudinaryFile(formData)
+        return url
+      })
+
+      const url = await Promise.all(allSecured_url)
+
+
+      // now store the image on firebase
+      await push(ref(db, "singlemsg"), {
+        whoSendMsgUid: auth.currentUser.uid,
+        whoSendMsgusername: auth.currentUser.displayName,
+        whoSendMsgEmail: auth.currentUser.email,
+        whoSendMsgProfile_picture: auth.currentUser.photoURL,
+        whoRecivedMsgUid: user.userUid,
+        whoRecivedMsgUserName: user.userName,
+        whoRecivedMsgEmail: user.userEmail,
+        whoRecivedMsgProfilePicture: user.userProfilePicture,
+        message: url,
+        createdAt: lib.getTimeNow(),
+      });
+
+    } catch (error) {
+      console.error('failed to upload image on cloudinary and firebase', error);
+    } finally {
+      setuploadImageLoading(false)
+      closeModal();
+      if (inputRef.current) {
+        inputRef.current.value = null
+      }
+    }
+  }
+
+  // handleSofDelte
+  const handleSofDelte = (msgKey) => {
+    setAllmsg((prev) => {
+      const newmsg = prev.filter((singlemsg) => singlemsg.msgKey != msgKey)
+      localStorage.setItem('nesmsg', JSON.stringify(newmsg))
+      return newmsg
+    }
+    )
+  }
+
+
+
 
   return (
     <div className="w-full bg-amber-200 h-[95dvh] ">
@@ -105,40 +184,70 @@ const Chat = () => {
 
           <div className="flex flex-col overflow-y-scroll h-[70vh]">
             {allMsg?.map((msg) => {
-              //ami  sender, user receiver
-              if (
-                auth.currentUser.uid === msg.whoSendMsgUid &&
-                msg.whoRecivedMsgUid === user.userUid
-              ) {
+              if (auth.currentUser.uid == msg.whoSendMsgUid && msg.whoRecivedMsgUid === user.userUid) {
                 return (
-                  <div key={msg.msgKey} className="self-end">
-                    <div className="flex flex-col items-end mt-7">
-                      <div className="px-5 w-full text-wrap py-3 bg-blue-300 rounded-3xl">
-                        <h2>{msg.message}</h2>
+                  Array.isArray(msg.message) ? (
+                    <div key={msg.msgKey} className="self-end">
+                      <div className="flex flex-col items-end mt-7">
+                        <div className="">
+                          <ImageLayout imageSrc={msg.message} />
+                        </div>
+                        <p>Today, 2:02pm</p>
+                      </div>
+                    </div>
+                  ) : (
+                    <div key={msg.msgKey} className="self-end">
+                      <div className="flex flex-col items-end mt-7">
+                        <div className="flex gap-x-4 items-center">
+                          <span onClick={() => {
+                            setselectedMsg(msg.msgKey)
+                            setremove(!remove)
+                          }} className="px-2 py-1 bg-gray-400  rounded-3xl ">:</span>
+                          {/* button Group */}
+                          {remove && selectedMsg == msg.msgKey && (
+                            <div>
+                              <button class="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900" onClick={() => handleSofDelte(msg.msgKey)}>Delete Soft</button>
+                              <button class="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-purple-900">Delete Hard</button>
+                            </div>
+                          )}
+
+                          <div className="px-5 w-full text-wrap py-3 bg-blue-300 rounded-3xl">
+
+                            <h2>{msg.message}</h2>
+                          </div>
+                        </div>
+                        <p>Today, 2:02pm</p>
+                      </div>
+                    </div>
+                  )
+
+
+
+                )
+
+                //   reciver jokhon sender, ami jokhon receiver
+              } else if (msg.whoSendMsgUid == user.userUid && msg.whoRecivedMsgUid == auth.currentUser.uid) {
+                return (
+                  Array.isArray(msg.message) ? (<div key={msg.msgKey} className="self-start">
+                    <div className="flex flex-col items-start mt-4">
+                      <div className="">
+                        <ImageLayout imageSrc={msg.message} />
                       </div>
                       <p>Today, 2:02pm</p>
                     </div>
-                  </div>
-                );
-              }
-              // User sender, ami receiver
-              else if (
-                msg.whoSendMsgUid === user.userUid &&
-                msg.whoRecivedMsgUid === auth.currentUser.uid
-              ) {
-                return (
-                  <div key={msg.msgKey} className="self-start">
+                  </div>) : (<div key={msg.msgKey} className="self-start">
                     <div className="flex flex-col items-start mt-4">
                       <div className="px-5 w-full text-wrap py-3 bg-gray-300 rounded-3xl">
                         <h2>{msg.message}</h2>
                       </div>
                       <p>Today, 2:02pm</p>
                     </div>
-                  </div>
-                );
+                  </div>)
+
+                )
               }
               // Onno message gulo show korbo na
-              return null;
+              return null
             })}
           </div>
           {/* chat view */}
@@ -163,7 +272,7 @@ const Chat = () => {
                 >
                   <FaRegSmile />
                 </span>
-                <span className="text-xl cursor-pointer">
+                <span className="text-xl cursor-pointer" onClick={() => openModal()}>
                   <FaCameraRetro />
                 </span>
               </div>
@@ -187,8 +296,48 @@ const Chat = () => {
           <EmojiPicker open={emojiOpen} onEmojiClick={handleEmoji} />
         </div>
         {/* emoji picker component  */}
+
+        {/* modal component */}
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          style={customStyles}
+          contentLabel="imageUpload Modal"
+        >
+
+
+
+          <div class="flex items-center justify-center w-full">
+            <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50  dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+              <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
+                </svg>
+                <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
+                <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+              </div>
+              <input id="dropzone-file" type="file" multiple onChange={(e) => setFieldImage([e.target.files])} ref={inputRef} />
+            </label>
+          </div>
+
+
+          <div className="flex justify-center items-center  mt-10">
+            {/* button group */}
+            <button type="button" class="text-white bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:focus:ring-yellow-900" onClick={() => closeModal()}>Cancle</button>
+            {uploadImageLoading ? (
+              <button type="button" class="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+              >Upload ....</button>) : (
+              <button type="button" class="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+                onClick={handleUploadImage}
+              >Upload</button>)}
+
+            {/* button group */}
+          </div>
+
+        </Modal>
+        {/* modal component */}
       </div>
-    </div>
+    </div >
   );
 };
 export default React.memo(Chat);
